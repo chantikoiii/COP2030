72b9e1139816209b358fc4a6a941c9b112877e28
John Westbrook <156149716+chantikoiii@users.noreply.github.com>
2024-11-29 23:56:32 -0500

    Update psuedo.java

--- COP2030/Asign7/psuedo.java
+++ COP2030/Asign7/psuedo.java
@@ -1,15 +1,17 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 public class psuedo{
-    public static void main(String [] args) throws FileNotFoundException {
+    public static void main(String [] args) throws IOException {
 
+        //declaring file as object
         File file = new File("your/directory/here");
 
+        //reads the text file one character at a time
         FileReader readMe = new FileReader(file);
+        //wrapping 'readMe' with a BufferedReader will allow file to be read one line at a time
         BufferedReader bufferedReadMe = new BufferedReader(readMe);
 
         try(bufferedReadMe){
@@ -20,8 +22,8 @@ public class psuedo{
                 System.out.println(line);
             }
 
-        } catch(IOException e){
-            System.err.println("An error has occured while reading document: " + e.getMessage());
+        } catch(IOException e){ //program keeps throwing 'FileNotFoundException' error for some reason
+            System.err.println("An error has occured " + e.getMessage());
         }
     }
 }
